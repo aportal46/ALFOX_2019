292818c26cabe75657b1ea70f267a183db04824f
snir2g1 <snir2g1@S92-P777>
2019-06-04 13:24:13 +0200

    MessageManager

--- src/java/com/persistence/MessageManager.java
+++ src/java/com/persistence/MessageManager.java
@@ -30,6 +30,29 @@ public class MessageManager {
     // message a envoyer
     private static String msg = ""; 
     
+    
+    public static final int NV_MODE = 0x30;
+    
+    public static enum Etat {VOID, NORMAL, DEGRADE, DMD_GPS, GPS, EC0,
+                                    MAINTENANCE, RESET, INIT, SLEEP, DATA };
+    
+    public static enum DmdMode {MODE_STANDARD, DMD_GPS, MODE_GPS, MODE_EC0,
+                                    MODE_MAINTENANCE, MODE_RESET, MODE_SLEEP, MODE_DATA, RIEN}; 
+    
+    private static String nouveauMode(int mode) {
+        byte[] bMsg = {0,0,0,0,0,0,0,0};
+        bMsg[0] = (byte) NV_MODE;
+        bMsg[1] = (byte)mode;
+        return new String(bMsg);
+    }
+    
+    public static void demandeDeMode (int mode) {
+        String msg = MessageManager.nouveauMode (mode);
+        if (!MessageManager.demandEnvoye (msg)) {
+            // .. error
+        }
+    }
+    
     /*
      * param 1 msg : message a envoyé, 8 octets sous la forme hexadecimale 
      * Cette méthode est appeler pour une demande d'envoie d'un message 
